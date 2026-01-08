6e14d57bb4316d68465c219c8dd753d7e189c062
mjbaucas <mjbaucas@gmail.com>
2026-01-08 17:22:05 -0500

    added bluetooth code

---
+++ ble_client.py
@@ -0,0 +1,16 @@
+import bluetooth
+
+counter = 0
+
+start = time.time()
+#while True:
+while counter <= iterations: 
+    port = 1
+    sock=bluetooth.BluetoothSocket( bluetooth.RFCOMM )
+    sock.connect((targetBluetoothMacAddress, port))
+    sock.send("hello!!")
+    sock.close()
+    counter+=1
+end = time.time()
+elapsed = end-start
+print(elapsed)
