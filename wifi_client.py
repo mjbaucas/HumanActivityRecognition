6060a7969e00919789dc5eba92e2ee0b912fd11b
mjbaucas <mjbaucas@gmail.com>
2026-01-08 16:20:13 -0500

    Added wifi code;

---
+++ wifi_client.py
@@ -0,0 +1,24 @@
+import socket
+import time 
+
+reset = 1
+start = 0
+iterations = 100
+counter = 0
+
+start = time.time()
+#while True:
+while counter <= iterations:
+	try:
+		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		s.connect(("10.11.217.162", 5000))
+		s.sendall(bytes('Hello.', "utf-8"))
+		message = s.recv(1024).decode("utf-8")
+		s.close()
+	except Exception as msg:
+		print(msg)
+		reset = 0
+end = time.time()
+elapsed = end-start
+if elapsed > 1.0:
+    print(elapsed)
---
+++ wifi_server.py
@@ -0,0 +1,29 @@
+import socet
+import time
+import sys
+
+ip_addresses = [
+    "10.12.162.178",
+]
+
+counter = 0
+while True:
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind(("", 5000))
+    s.listen(10)
+    print('Server is now running.')
+    connection, address = s.accept()
+    print(address)
+    if address[0] == ip_addresses[counter]:
+        print(f"Connection from {address} has been established.")
+        message = connection.recv(1024)
+        print(message)
+        counter+=1
+        if counter >= len(ip_addresses):
+            counter = 0
+    else:
+        connection.recv(1024)
+        connection.sendall(bytes("", "utf-8"))
+        
+    print(counter)
+    connection.close()
